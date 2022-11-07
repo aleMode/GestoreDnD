3ebd4f30a2fa46fb5f7069bd9a5b84f1c66b690a
aleMode <ale.modena1@gmail.com>
2022-11-07 23:17:21 +0100

    controllo password e mail uguali in registrazione

--- app/src/main/java/com/example/gestorednd/Login.kt
+++ app/src/main/java/com/example/gestorednd/Login.kt
@@ -11,11 +11,15 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.gestorednd.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -26,6 +30,13 @@ class Login : AppCompatActivity() {
 
         val registrami = findViewById<TextView>(R.id.txtRegistra)
 
+        auth = Firebase.auth //Initialize Firebase Auth
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            // INSERISCI PAGINA DOPO
+        }
         registrami.setOnClickListener {
            val intent = Intent(this, Registration::class.java)
            startActivity(intent)
--- app/src/main/java/com/example/gestorednd/Registration.kt
+++ app/src/main/java/com/example/gestorednd/Registration.kt
@@ -2,19 +2,20 @@ package com.example.gestorednd
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.gestorednd.databinding.ActivityRegistrationBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Registration : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityRegistrationBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +23,49 @@ class Registration : AppCompatActivity() {
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val login = findViewById<TextView>(R.id.txtLogin)
+        auth = Firebase.auth //Initialize Firebase Auth
 
+        val login = findViewById<TextView>(R.id.txtLogin)
         login.setOnClickListener {
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
 
-    }
+        var register = findViewById<Button>(R.id.btnRegistra);
+        var email = findViewById<TextView>(R.id.txtEmail)
+        var emailConf = findViewById<TextView>(R.id.txtEmailConf)
+        var password = findViewById<TextView>(R.id.txtPwd)
+        var passwordConf = findViewById<TextView>(R.id.txtPwdConf)
+
+        register.setOnClickListener {
+            //INSERISCI CONTROLLI
+            if(email.text != emailConf.text) findViewById<TextView>(R.id.txtError).text = "Indirizzi email diversi"
+            else{
+                if(password.text != password.text) findViewById<TextView>(R.id.txtError).text = "Password diversw"
+                else{
+                    auth.createUserWithEmailAndPassword(email.text as String, password.text as String)
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                // Sign in success, update UI with the signed-in user's information
+                                //Log.d(TAG, "createUserWithEmail:success")
+                                //val user = auth.currentUser
+                                //updateUI(user)
+                            } else {
+                                // If sign in fails, display a message to the user.
+                               // Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                                //Toast.makeText(baseContext, "Authentication failed.",
+                                    //Toast.LENGTH_SHORT).show()
+                                //updateUI(null)
+                            }
+                        }
+                }
+                }
+            }
+
+        }
+
+
+
+
 
 }
